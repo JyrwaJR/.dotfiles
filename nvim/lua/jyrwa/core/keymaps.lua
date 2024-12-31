fa7632563be2ed67e53142c981d34a8231064d02
JyrwaJr <jyrwaboys@gmail.com>
2024-12-31 14:33:22 +0530

    change delete key map

--- nvim/lua/jyrwa/core/keymaps.lua
+++ nvim/lua/jyrwa/core/keymaps.lua
@@ -5,7 +5,7 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
+keymap.set("n", "dd", '"_dd', { noremap = true, desc = "Delete line" })
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
