e34bc1c05e37e0f7013649ba52f1e0140aef1f68
JyrwaJr <jyrwaboys@gmail.com>
2025-01-31 12:48:25 +0530

    add file browser

--- nvim/lua/jyrwa/core/keymaps.lua
+++ nvim/lua/jyrwa/core/keymaps.lua
@@ -26,6 +26,7 @@ keymap.set("n", "<leader>cn", "<cmd>NoiceDismiss<CR>", { desc = "Close all notif
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 keymap.set("n", "<leader>fo", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>fb", "<cmd>Telescope files_browser<cr>", { desc = "File Browser" })
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 keymap.set("n", "<leader>fn", "<cmd>Telescope noice<cr>", { desc = "Fuzzy find noice" })
