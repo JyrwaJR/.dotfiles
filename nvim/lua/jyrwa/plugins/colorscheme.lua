c994b64f4465d0cc8007f0c89c536f10b8b9eea1
JyrwaJR <jyrwaboys@gmail.com>
2025-12-05 15:31:13 +0530

    update

--- nvim/lua/jyrwa/plugins/colorscheme.lua
+++ nvim/lua/jyrwa/plugins/colorscheme.lua
@@ -4,10 +4,10 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
       background = {
         light = "macchiato",
-        dark = "mocha",
+        dark = "macchiato",
       },
       float = { border = "rounded", transparent = true, solid = true },
       transparent_background = true, -- Modern transparent background
