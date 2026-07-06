18b884f0bea972d78d200731f380f8993717156b
JyrwaJR <jyrwaboys@gmail.com>
2026-07-06 16:15:33 +0530

    feat: make noctis background transparent via Normal/NormalFloat highlight override

--- nvim/lua/jyrwa/plugins/colorscheme.lua
+++ nvim/lua/jyrwa/plugins/colorscheme.lua
@@ -261,5 +261,9 @@ return {
     })
 
     vim.cmd.colorscheme("noctis")
+
+    -- Transparent background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
   end,
 }
