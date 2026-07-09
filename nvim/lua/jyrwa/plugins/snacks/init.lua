ee17753a57b1bfa955cf8bf346f23beffd4741c9
JyrwaJR <jyrwaboys@gmail.com>
2026-07-09 21:38:46 +0530

    feat(snacks): add modular sub-module config files
    Create init.lua as the central config merger, plus separate modules for
    dashboard, notifier, picker, and all keymaps.

---
+++ nvim/lua/jyrwa/plugins/snacks/init.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+function M.get_opts()
+  return {
+    bigfile = { enabled = true, notify = false },
+    dashboard = require("jyrwa.plugins.snacks.dashboard"),
+    indent = { enabled = true },
+    input = {
+      enabled = true,
+      win = {
+        style = "input",
+        relative = "cursor",
+        row = 1,
+        col = 0,
+        width = 30,
+      },
+    },
+    notifier = require("jyrwa.plugins.snacks.notifier"),
+    picker = require("jyrwa.plugins.snacks.picker"),
+    quickfile = { enabled = true },
+    rename = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    terminal = { enabled = true },
+    words = { enabled = true },
+    zen = { enabled = true },
+    styles = {
+      notification = {
+        wo = { wrap = true },
+      },
+    },
+  }
+end
+
+function M.get_keys()
+  return require("jyrwa.plugins.snacks.keys")
+end
+
+return M
---
+++ nvim/lua/jyrwa/plugins/snacks/keys.lua
@@ -0,0 +1,114 @@
+return {
+  -- Top Level Utilities
+  {
+    "<leader>.",
+    function()
+      Snacks.scratch()
+    end,
+    desc = "Toggle Scratch Buffer",
+  },
+  {
+    "<leader>S",
+    function()
+      Snacks.scratch.select()
+    end,
+    desc = "Select Scratch Buffer",
+  },
+  {
+    "<leader>n",
+    function()
+      Snacks.notifier.show_history()
+    end,
+    desc = "Notification History",
+  },
+  {
+    "<leader>bd",
+    function()
+      Snacks.bufdelete()
+    end,
+    desc = "Delete Buffer",
+  },
+  {
+    "<leader>cR",
+    function()
+      Snacks.rename.rename_file()
+    end,
+    desc = "Rename File",
+  },
+  {
+    "<leader>un",
+    function()
+      Snacks.notifier.hide()
+    end,
+    desc = "Dismiss All Notifications",
+  },
+  {
+    "<leader>nd",
+    function()
+      Snacks.notifier.hide()
+    end,
+    desc = "Dismiss All Notifications",
+  },
+
+  -- Git
+  {
+    "<leader>gg",
+    function()
+      Snacks.lazygit()
+    end,
+    desc = "Lazygit",
+  },
+  {
+    "<leader>gl",
+    function()
+      Snacks.lazygit.log()
+    end,
+    desc = "Lazygit Log",
+  },
+  {
+    "<leader>gB",
+    function()
+      Snacks.gitbrowse()
+    end,
+    desc = "Git Browse",
+  },
+  {
+    "<leader>gb",
+    function()
+      Snacks.git.blame_line()
+    end,
+    desc = "Git Blame Line",
+  },
+  {
+    "<leader>gf",
+    function()
+      Snacks.lazygit.log_file()
+    end,
+    desc = "Lazygit Current File Log",
+  },
+
+  -- Navigation & View
+  {
+    "<leader>z",
+    function()
+      Snacks.zen()
+    end,
+    desc = "Toggle Zen Mode",
+  },
+  {
+    "<leader>u",
+    function()
+      Snacks.picker.undo()
+    end,
+    desc = "Undo History",
+  },
+
+  -- Terminal
+  {
+    "<leader>tt",
+    function()
+      Snacks.terminal()
+    end,
+    desc = "Toggle Terminal",
+  },
+}
