691027f4e1b11e8f83a371b546cd33f7e769b4b7
JyrwaJR <jyrwaboys@gmail.com>
2026-07-13 21:09:35 +0530

    refactor: replace kulala.nvim with rest.nvim
    Migrate from mistweaverco/kulala.nvim to rest-nvim/rest.nvim:
    - Remove plenary.nvim dependency (not needed by rest.nvim)
    - Map existing keymaps to rest.nvim API equivalents
      - run() preserved, last() replaces replay()
      - Add Rest open and Rest env show keymaps
      - Drop run_all/copy/inspect/show_env (no rest.nvim equivalent)
    - Replace opts table with config function setting vim.g.rest_nvim
    - Configure request, response, highlight, UI, cookies, and env options

--- nvim/lua/jyrwa/plugins/rest.lua
+++ nvim/lua/jyrwa/plugins/rest.lua
@@ -1,35 +1,22 @@
 return {
-  "mistweaverco/kulala.nvim",
+  "rest-nvim/rest.nvim",
   ft = { "http", "rest" },
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
   keys = {
-    -- Same keymaps as rest.nvim
     {
       "<leader>kr",
       function()
-        require("kulala").run()
+        require("rest-nvim").run()
       end,
       desc = "Run HTTP request",
     },
     {
-      "<leader>ka",
-      function()
-        require("kulala").run_all()
-      end,
-      desc = "Run all HTTP requests",
-    },
-    {
       "<leader>kl",
       function()
-        require("kulala").replay()
+        require("rest-nvim").last()
       end,
       desc = "Replay last request",
     },
-
     {
       "<leader>kp",
       function()
@@ -38,62 +25,48 @@ return {
       end,
       desc = "Open scratchpad",
     },
-
-    {
-      "<leader>kc",
-      function()
-        require("kulala").copy()
-      end,
-      desc = "Copy as cURL",
-    },
-    {
-      "<leader>ki",
-      function()
-        require("kulala").inspect()
-      end,
-      desc = "Inspect request",
-    },
-    {
-      "<leader>ks",
-      function()
-        require("kulala").show_env()
-      end,
-      desc = "Show env",
-    },
     {
       "<leader>ko",
-      function()
-        require("kulala").open()
-      end,
+      "<cmd>Rest open<CR>",
       desc = "Open last response",
     },
+    {
+      "<leader>ke",
+      "<cmd>Rest env show<CR>",
+      desc = "Show active env file",
+    },
   },
 
-  opts = {
-    global_keymaps = false,
-
-    -- UI
-    ui = {
-      display_mode = "float",
-      win_opts = {
-        border = "rounded",
+  -- rest.nvim uses vim.g.rest_nvim instead of a .setup() function.
+  -- We set the global here via lazy.nvim's config hook.
+  config = function()
+    ---@type rest.Config
+    vim.g.rest_nvim = {
+      request = {
+        skip_ssl_verification = true,
+        hooks = {
+          encode_url = true,
+        },
       },
-    },
-
-    -- Local development
-    default_env = "dev",
-    debug = false,
-
-    -- Request options
-    request_timeout = 10000,
-    additional_curl_options = {
-      "-k",
-      "--noproxy",
-      "localhost,127.0.0.1",
-    },
-
-    -- Response
-    show_request_summary = false,
-    show_icons = true,
-  },
+      response = {
+        hooks = {
+          decode_url = true,
+          format = true,
+        },
+      },
+      highlight = {
+        enable = true,
+        timeout = 750,
+      },
+      ui = {
+        winbar = true,
+      },
+      cookies = {
+        enable = true,
+      },
+      env = {
+        enable = true,
+      },
+    }
+  end,
 }
