c1eca1d833d7aeaf249310eba683e402a90b1d01
JyrwaJR <jyrwaboys@gmail.com>
2026-07-26 22:00:07 +0530

    feat: enhance refactoring.nvim with 10 dedicated keymaps
    Add direct keymaps for every refactoring operation and debug print
    tools, replacing the single select-refactor-menu approach:
    
    - extract variable (<leader>re), function (<leader>rf), to file (<leader>rF)
    - inline variable (<leader>ri), function (<leader>rI)
    - debug: print var (<leader>rp), expression (<leader>rx), location (<leader>rl)
    - debug: cleanup prints (<leader>rc)
    - keep menu fallback (<leader>r v-mode)

--- nvim/lua/jyrwa/plugins/refactoring.lua
+++ nvim/lua/jyrwa/plugins/refactoring.lua
@@ -1,6 +1,87 @@
 return {
   "ThePrimeagen/refactoring.nvim",
   keys = {
+    -- Visual Mode: Extract
+    {
+      "<leader>re",
+      function()
+        require("refactoring").extract_var({ show_success_message = true })
+      end,
+      mode = "v",
+      desc = "Extract variable",
+    },
+    {
+      "<leader>rf",
+      function()
+        require("refactoring").extract_func({ show_success_message = true })
+      end,
+      mode = "v",
+      desc = "Extract function",
+    },
+    {
+      "<leader>rF",
+      function()
+        require("refactoring").extract_func_to_file({ show_success_message = true })
+      end,
+      mode = "v",
+      desc = "Extract function to file",
+    },
+
+    -- Normal Mode: Inline
+    {
+      "<leader>ri",
+      function()
+        require("refactoring").inline_var({ show_success_message = true })
+      end,
+      mode = "n",
+      desc = "Inline variable",
+    },
+    {
+      "<leader>rI",
+      function()
+        require("refactoring").inline_func({ show_success_message = true })
+      end,
+      mode = "n",
+      desc = "Inline function",
+    },
+
+    -- Visual Mode: Debug Print
+    {
+      "<leader>rp",
+      function()
+        require("refactoring").debug.print_var({})
+      end,
+      mode = "v",
+      desc = "Debug: print variable(s)",
+    },
+    {
+      "<leader>rx",
+      function()
+        require("refactoring").debug.print_exp({})
+      end,
+      mode = "v",
+      desc = "Debug: print expression",
+    },
+    {
+      "<leader>rc",
+      function()
+        require("refactoring").debug.cleanup({})
+      end,
+      mode = "v",
+      desc = "Debug: cleanup prints",
+    },
+
+    -- Normal Mode: Debug Print
+    {
+      "<leader>rl",
+      function()
+        require("refactoring").debug.print_loc({})
+      end,
+      mode = "n",
+      desc = "Debug: print location",
+    },
+
+    -- Fallback: Select refactor menu
     {
       "<leader>r",
       function()
@@ -9,9 +90,7 @@ return {
         })
       end,
       mode = "v",
-      noremap = true,
-      silent = true,
-      expr = false,
+      desc = "Select refactor...",
     },
   },
   opts = {
